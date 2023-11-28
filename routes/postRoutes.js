13691b0648279044e0113432655f250f1ff74196
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-11-28 15:07:13 -0300

    post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,4 +6,33 @@ import Post from '../mongodb/models/post.js';
 
 dotenv.config();
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+
+//Create a post
+router.route('/').post(async (req, res) => {
+
+    try {
+        const { name, prompt, photo } = req.body;
+
+        const photoUrl = await cloudinary.uploader.upload(photo);
+
+        const newPost = await Post.create({
+            name,
+            prompt,
+            photo: photoUrl.url,
+        });
+
+        res.status(201).json({ success: true, data: newPost });
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: error });
+    }
+})
+
 const router = express.Router();
